152f08a76f553eea2fcd276d0cbb4ace3b2e3dcb
victor <parhom1805@gmail.com>
2024-05-06 12:48:57 +0200

    Update hw_6.py

--- hw_6.py
+++ hw_6.py
@@ -58,12 +58,39 @@ class Record:
 class AddressBook(UserDict):
     # Клас для зберігання та управління записами.
     # Додавання Пошук Видалення за іменем
-    def add_record(self,name):
-        self.data[name] = Record(name)
+    def add_record(self, record):
+        self.data[record.name.value] = record
         
     def find(self, name):
         return self.data.get(name)
     
     def delete(self, name):
         del self.data[name]
+        
+book = AddressBook()
 
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+    
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+print(john)
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")
+
+book.delete("Jane")
+for name, record in book.data.items():
+    print(record)
